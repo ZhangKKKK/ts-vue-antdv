22676f57b99628fbdd34cf59c52c3842a1fa5a74
zhc <zhanghengchang@netjoy.com>
2021-03-09 15:17:01 +0800

    add ant zhCN

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <a-config-provider :locale="locale">
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </a-config-provider>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { PeimessionMoudle } from '@/store/module/peimission'
+import { ConfigProvider } from 'ant-design-vue'
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
 
-@Component
+@Component({
+  components: { 'a-config-provider': ConfigProvider }
+})
 export default class App extends Vue {
+  private locale: any = zhCN
   public created () {
     PeimessionMoudle.GenerateRoutes(['sale'])
     // this.$store.dispatch('GenerateRoutes', ['sale'])
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,8 @@ declare module '*.vue' {
 }
 
 declare module 'mockjs';
+
+declare module 'ant-design-vue/lib/locale-provider/zh_CN' {
+  const zh_CN: any
+  export default zh_CN
+};
